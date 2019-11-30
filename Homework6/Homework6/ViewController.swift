2a035146eb16d82f44bc9232038baa389ac3299d
cramsayiv <37635288+cramsayiv@users.noreply.github.com>
2019-11-29 18:41:13 -0600

    complete homework 6

--- Homework6/Homework6/ViewController.swift
+++ Homework6/Homework6/ViewController.swift
@@ -9,19 +9,61 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var conditonOne: Bool = false
+    var conditionTwo: Bool = true
+    var conditionThree: Bool = false
+    
 
     @IBOutlet var segmentedController: UISegmentedControl!
     @IBOutlet var selectionSwitch: UISwitch!
     @IBOutlet var selectionButton: UIButton!
     @IBOutlet var continueButton: UIButton!
     
-    @IBAction func segmentedControllerDidChangeValue(_ sender: UISegmentedControl) {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        continueButton.isEnabled = false
+        // Do any additional setup after loading the view.
     }
     
+    func checkAllConditions(){
+        if conditonOne && conditionTwo && conditionThree {
+            continueButton.isEnabled = true
+        }
+    }
+    
+    
+    @IBAction func segmentedControllerDidChangeValue(_ sender: UISegmentedControl) {
+        
+        if sender.selectedSegmentIndex == 1 {
+        conditonOne = true
+        } else{
+            conditonOne = false
+        }
+        
+        checkAllConditions()
+        
+    }
+
+    
     @IBAction func selectionSwitchDidChangeValue(_ sender: UISwitch) {
+        if sender.isOn {
+            
+            conditionTwo = true
+        } else {
+            conditionTwo = false
+        }
+        
+        checkAllConditions()
+        
     }
     
     @IBAction func selectionButtonTapped(_ sender: UIButton) {
+        
+        conditionThree = true
+        
+        checkAllConditions()
+        
     }
-}
 
+}
